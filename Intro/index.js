9557df2b290b4eb2d8b98121e4a8ff0b6094a559
Jepp0103 <jeppendyekjaer@hotmail.com>
2020-07-02 14:14:53 +0200

    :..

---
+++ Intro/index.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+
+
+let name = "Jeppe"; 
+
+console.log(name);
+
+//Cannot be a reserved keyword
+//Should be meaningful
+//Don't start with a number
+//Cannot contain a space or hyphen
+//Are case-sensitive
+
+let firstName = "Martin", secondName = "Harry"; //Declare variables under same identifier.
+//Best practice
+let middleName = "Nannestad";
+let surName = "Dyekjaer";
+
+console.log(firstName, secondName);
+console.log(middleName, surName);
+
+const interestRate = 0.3; //Can't change the value , hence type error.
+interestRate = 1;
+console.log(interestRate);
+
